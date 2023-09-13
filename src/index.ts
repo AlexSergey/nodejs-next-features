1933cf367fe138433436da42d64b4cc4240a5087
Serhii Aleksandrov <gooddev.sergey@gmail.com>
2023-09-13 15:01:17 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,30 @@ import { parseArgs } from 'node:util';
 
 import { taxCalculator } from './tax-calculator.js';
 import { logger } from './logger';
+import * as process from 'process';
 
 logger.silly('Calculation in progress');
 
-const res = await fetch('http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000');
-const [tax]: [number] = await res.json();
+try {
+  const res = await fetch('https://random-data-api.com/api/v2/users');
+  const [tax]: [number] = await res.json();
+  const args = process.argv.slice(2);
 
-const args = process.argv.slice(2);
+  const options = {
+    cost: {
+      type: 'string',
+      short: 'c'
+    }
+  };
 
-const options = {
-  cost: {
-    type: 'string',
-    short: 'c'
-  }
-};
+  const {
+    values,
+  } = parseArgs<any>({ args, options });
+  const cost = Number(values.cost);
 
-const {
-  values,
-} = parseArgs<any>({ args, options });
-const cost = Number(values.cost);
+  console.log(taxCalculator(cost, tax));
+} catch (e) {
+  console.error(e);
+}
 
-console.log(taxCalculator(cost, tax));
+console.log(`env: ${process.env.TEST}`)
